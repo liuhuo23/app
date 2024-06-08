0165d4eb82c6d98e7195f32af1007b63767c99fd
liuhuo <liuhuo2370@gmail.com>
2024-06-09 01:28:14 +0800

    feat: update

--- stu_app/src/main.rs
+++ stu_app/src/main.rs
@@ -1,3 +1,10 @@
+use std::{path::PathBuf, str::FromStr};
+
 fn main() {
     println!("Hello, world!");
+    let res = walkfile::walk(&PathBuf::from_str("./").unwrap()).unwrap();
+    for dir in res{
+        let (root, child_dirs, child_files) = dir.as_tuple_ref();
+        println!("{}, {:?}, {:?}", root.display(), child_dirs, child_files);
+    }
 }
